c527625e1bbd81c15804993087fd75ce44565d5b
evangeldaping <evangeldaping@gmail.com>
2020-02-22 13:24:17 +0800

    with authentication

---
+++ app/models/warehouse.rb
@@ -0,0 +1,13 @@
+class Warehouse < ApplicationRecord
+  has_many :stocks, dependent: :destroy
+  has_many :products, through: :stocks
+  has_many :orders, dependent: :destroy
+
+  validates :street, presence: true
+  validates :city, presence: true
+  validates :province, presence: true
+
+  def full_address
+    "#{street}, #{city}, #{province}"
+  end
+end
